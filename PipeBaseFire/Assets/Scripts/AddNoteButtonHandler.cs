22a1896264d5af7383da2d6bc3d426b48f30b080
Kyle Zhou <kylemsguy@users.noreply.github.com>
2017-09-16 19:38:52 -0400

    Firebase auth

---
+++ PipeBaseFire/Assets/Scripts/AddNoteButtonHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class Note
+{
+    /*
+     {
+        "id": "uuid",
+        "location": [lat, long],
+        "note": "note text"
+     }
+     */
+
+    public string id;
+    public double loclat;
+    public double loclong;
+    public string note;
+}
+
+public class AddNoteButtonHandler : MonoBehaviour {
+
+    private Button btn;
+
+    // Use this for initialization
+    void Start () {
+        btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick()
+    {
+
+    }
+}
---
+++ PipeBaseFire/Assets/Scripts/FireBaseAuthHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Auth;
+
+public class FireBaseAuthHandler : MonoBehaviour {
+
+    FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+
+    private Button btn;
+    private InputField usernameField;
+    private InputField passwordField;
+
+    // Use this for initialization
+    void Start()
+    {
+        btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        usernameField = GameObject.Find("UsernameField").GetComponent<InputField>();
+        passwordField = GameObject.Find("PasswordField").GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TaskOnClick()
+    {
+        string email = usernameField.text;
+        string password = passwordField.text;
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
